0870d8c8fc1edddbbd2d57473d4add55d914f4dd
FMendesss <fernando.mendesss@gmail.com>
2021-02-11 17:32:38 -0300

    implementando exercicio 3

--- Aula1/src/Exercicios/Aplicacao3.java
+++ Aula1/src/Exercicios/Aplicacao3.java
@@ -1,4 +1,11 @@
 package Exercicios;
 
 public class Aplicacao3 {
+
+    public static void main(String[] args) {
+        NotasAlunos = new NotasAlunos("Fernando", 8,9 );
+
+        System.out.println(NotasAlunos.media());
+
+    }
 }
--- Aula1/src/Exercicios/NotasAlunos.java
+++ Aula1/src/Exercicios/NotasAlunos.java
@@ -1,4 +1,23 @@
 package Exercicios;
 
 public class NotasAlunos {
+    private String nome;
+    private float nota1;
+    private float nota2;
+
+    //Construtor
+
+    public NotasAlunos(String nome, float nota1, float nota2) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    //Metodo
+
+    public void media(float media){
+        media = (nota1 + nota2)/2;
+    }
+
 }
+
